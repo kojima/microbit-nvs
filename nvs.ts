cc2d54c6d41c76cbb20843bc5aa9de62495fba18
kojima <hide.k0712@gmail.com>
2024-10-29 17:01:31 +0900

    add error handlings

--- nvs.ts
+++ nvs.ts
@@ -10,7 +10,11 @@ namespace nvs {
     //% blockId="flash pairs stored" block="flash pairs stored"
     //% shim=nvs::size
     export function size(): number {
-        return simMap ? simMap.size : 0;
+        try {
+            return simMap ? simMap.size : 0;
+        } catch {
+            return 0;
+        }
     }
 
     //% blockId="flash remove key" block="flash remove key %key"
@@ -22,13 +26,19 @@ namespace nvs {
     //% blockId="flash get buffer of key with size" block="flash get buffer of key %key with size %dataSize"
     //% shim=nvs::getBuffer
     export function getBuffer(key: string, dataSize: number): Buffer {
-        return simMap ? simMap[key] : -1;
+        try {
+            return simMap ? simMap[key] : null;
+        } catch {
+            return null;
+        }
     }
 
     //% blockId="flash put buffer for key" block="flash key %key's buffer as %data"
     //% shim=nvs::putBuffer
     export function putBuffer(key: string, data: Buffer) {
-        if (!simMap) simMap = {};
-        simMap[key] = data.toString();
+        try {
+            if (!simMap) simMap = {};
+            simMap[key] = data.toString();
+        } catch {}
     }
 }
